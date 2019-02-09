bac855c27c82deda4c731725669ce03732acd21d
jordanmceachern <jormceachern@gmail.com>
2019-02-09 08:23:19 -0400

    imageloader complete

---
+++ src/components/layout.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { Link } from 'gatsby'
+import './layout.css'
+import facebook from './facebook.png'
+import logo from './logo.jpg'
+
+class Layout extends Component {
+state = {
+    logoLoaded: false
+}
+
+componentDidMount(){
+    const img = new Image()
+    img.src = logo
+    img.onload = () => {this.setState({logoLoaded: true})}
+}
+
+toggleHandler = () => {
+    const navhtml = document.getElementsByClassName("mobile")
+    const navArr = Array.from(navhtml)
+    navArr.forEach(element => element.classList.toggle("hideThis"))
+}
+    
+render(){
+    let logoPlaceHolder = "William McQuinn"
+    if(this.state.logoLoaded){logoPlaceHolder = <img src={logo} alt="brand logo"/>}
+
+    return (
+        <nav>
+            <div id="hamburger" onClick={this.toggleHandler}>
+                <hr />
+                <hr />
+                <hr />
+            </div>
+            <Link id="home" to="/">{logoPlaceHolder}</Link>
+            <Link className="heading mobile hideThis" to="/aboutme">about me</Link>
+            <Link className="heading mobile hideThis" to="/photos">stills</Link>
+            <Link className="heading mobile hideThis" to="/videos">videos</Link>
+            <Link className="heading mobile hideThis" to="/contact">contact</Link>
+            <div id="facebook" className="heading mobile hideThis" onClick={this.toggleHandler}>
+                <a 
+                    href="https://www.facebook.com/williammcquinn7" 
+                    target="_blank" 
+                    rel="noopener noreferrer">
+                    <img id="fb" src={facebook} alt="link to facebook"/>facebook page
+                </a>
+            </div>
+        </nav>
+        )
+    }
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,12 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import Layout from '../components/layout.css'
+import ImageLoader from '../components/imageLoader/imageLoader'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+export default () => {
+  return (
+    <div className='App'>
+      <ImageLoader />
+      <Layout />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
-
-export default IndexPage
+  )
+}
